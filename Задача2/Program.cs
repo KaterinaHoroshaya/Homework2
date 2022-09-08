91293f0de1e00241939854893b56f0b24e766890
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-08 23:10:00 +0700

    Вывод третьего числа

---
+++ Задача2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int tmp=num;
+            int digs= (int)Math.Log10(num)+1;
+            if (digs<3) Console.WriteLine("третьей цифры нет");
+            else
+            {
+                tmp = num / (int)Math.Pow(10, digs - 3);
+                num = tmp % 10;
+                Console.WriteLine(num);
+            }
