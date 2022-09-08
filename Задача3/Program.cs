95298652204825c361b83031c0627327c6aef138
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-08 23:04:36 +0700

    Определение выходного

---
+++ Задача3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите номер дня недели: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A >= 1 && A < 6)
+{
+    Console.WriteLine("No");
+}
+if (A >= 6 && A <= 7)
+{
+    Console.WriteLine("Yes");
+}
+if (A < 1)
+{
+    Console.WriteLine("Это не номер дня недели");
+}
+if (A > 7)
+{
+    Console.WriteLine("Это не номер дня недели");
+}
